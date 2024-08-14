65349205b8a6b23c7610b99d49e6ba337a1989d3
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2024-08-14 20:55:27 -0300

    introduction leds up

--- src/components/BpmnViewer.js
+++ src/components/BpmnViewer.js
@@ -1,9 +1,10 @@
 // src/components/BpmnViewer.js
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import BpmnJS from 'bpmn-js';
 
 const BpmnViewer = ({ diagramUrl }) => {
   const viewerRef = useRef(null);
+  const [height, setHeight] = useState('800px');
 
   useEffect(() => {
     const bpmnViewer = new BpmnJS({
@@ -14,9 +15,12 @@ const BpmnViewer = ({ diagramUrl }) => {
       .then(response => response.text())
       .then(xml => bpmnViewer.importXML(xml))
       .then(() => {
-        // Ajusta a escala para que o diagrama caiba na tela
         const canvas = bpmnViewer.get('canvas');
         canvas.zoom('fit-viewport');
+
+        const viewbox = canvas.viewbox();
+        const calculatedHeight = viewbox.height;
+        setHeight(`${calculatedHeight}px`);
       })
       .catch(error => console.error('Error loading BPMN diagram:', error));
 
@@ -25,7 +29,7 @@ const BpmnViewer = ({ diagramUrl }) => {
     };
   }, [diagramUrl]);
 
-  return <div ref={viewerRef} style={{ width: '100%', height: '800px' }} />;
+  return <div ref={viewerRef} style={{ width: '100%', height }} />;
 };
 
 export default BpmnViewer;
