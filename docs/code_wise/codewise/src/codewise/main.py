3a51e97eec2942fe0d98a5520d952e9d95c2bcd9
bs20242 <bsimao11@gmail.com>
2025-05-26 17:57:06 -0300

    mudanças agents e tasks e diretorios

--- docs/code_wise/codewise/src/codewise/main.py
+++ docs/code_wise/codewise/src/codewise/main.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python
 import sys
+import os
 import warnings
-
 from datetime import datetime
 
-from codewise.crew import Codewise
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from crew import Codewise
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-#test
 def run():
-    """
-    Run the crew.
-    """
     inputs = {
         'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'current_year': str(datetime.now().year),
+        'project_path': './'  # ou o caminho real do código
     }
-    
     try:
         Codewise().crew().kickoff(inputs=inputs)
     except Exception as e:
@@ -29,37 +23,28 @@ def run():
 
 
 def train():
-    """
-    Train the crew for a given number of iterations.
-    """
     inputs = {
         "topic": "AI LLMs"
     }
     try:
         Codewise().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
 
 def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
     try:
         Codewise().crew().replay(task_id=sys.argv[1])
-
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
 
 def test():
-    """
-    Test the crew execution and returns the results.
-    """
     inputs = {
         "topic": "AI LLMs"
     }
     try:
         Codewise().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
+
+if __name__ == "__main__":
+    run()
